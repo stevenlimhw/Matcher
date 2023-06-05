08e027f5331bc40f9f08eefe7f37eace70a6806c
Steven Lim <limhwsteven@gmail.com>
2023-06-05 21:47:53 +0800

    Add OrderCore

--- Orders/IOrderCore.cs
+++ Orders/IOrderCore.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace TradingEngineServer.Orders
 {
     public interface IOrderCore
     {
-        
+        public long OrderId { get; }
+        public string Username { get; }
+        public int SecurityId { get; }
     }
 }
