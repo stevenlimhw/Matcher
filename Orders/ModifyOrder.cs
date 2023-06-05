3edfff76c8fa5f188f4d4a022f975774351f1ae0
Steven Lim <limhwsteven@gmail.com>
2023-06-05 22:22:00 +0800

    Add order entities

---
+++ Orders/ModifyOrder.cs
@@ -0,0 +1,32 @@
+namespace TradingEngineServer.Orders
+{
+    public class ModifyOrder : IOrderCore
+    {
+        public ModifyOrder(IOrderCore orderCore, long modifyPrice, uint modifyQuantity, bool isBuySide)
+        {
+            Price = modifyPrice;
+            Quantity = modifyQuantity;
+            IsBuySide = isBuySide;
+            
+            _orderCore = orderCore;
+        }
+
+        public long Price { get; private set; }
+        public uint Quantity { get; private set; }
+        public bool IsBuySide { get; private set; }
+
+        public long OrderId => _orderCore.OrderId;
+        public string Username => _orderCore.Username;
+        public int SecurityId => _orderCore.SecurityId;
+        public CancelOrder ToCancelOrder()
+        {
+            return new CancelOrder(this);
+        }
+        public Order ToNewOrder()
+        {
+            return new Order(this);
+        }
+
+        private readonly IOrderCore _orderCore;
+    }
+}
---
+++ Orders/Order.cs
@@ -0,0 +1,43 @@
+namespace TradingEngineServer.Orders
+{
+    public class Order : IOrderCore
+    {
+        public Order(IOrderCore orderCore, long price, uint quantity, bool isBuySide)
+        {
+            Price = price;
+            InitialQuantity = quantity;
+            CurrentQuantity = quantity;
+            IsBuySide = isBuySide;
+
+            _orderCore = orderCore;
+        }
+
+        public Order(ModifyOrder modifyOrder) : this(modifyOrder, modifyOrder.Price, modifyOrder.Quantity, modifyOrder.IsBuySide)
+        {
+
+        }
+
+        public long Price { get; private set; }
+        public uint InitialQuantity { get; private set; }
+        public uint CurrentQuantity { get; private set; }
+        public bool IsBuySide { get; private set; }
+        public long OrderId => _orderCore.OrderId;
+        public string Username => _orderCore.Username;
+        public int SecurityId => _orderCore.SecurityId;
+
+        public void IncreaseQuantity(uint quantityDelta) 
+        {
+            CurrentQuantity += quantityDelta;
+        }
+
+        public void DecreaseQuantity(uint quantityDelta) 
+        {
+            if (quantityDelta > CurrentQuantity)
+                throw new InvalidOperationException($"Quantity Delta > Current Quantity for OrderId = {OrderId}");
+            
+            CurrentQuantity -= quantityDelta;
+        }
+        
+        private readonly IOrderCore _orderCore;
+    }
+}
