812f6b9e86cd02f06a3acccdb859d7c2962981de
Steven Lim <limhwsteven@gmail.com>
2023-06-11 17:23:23 +0800

    Add trade class

--- Matcher/Trades/Trade.cs
+++ Matcher/Trades/Trade.cs
@@ -2,6 +2,12 @@ namespace TradingEngineServer.Matcher
 {
     public class Trade
     {
-        
+        public Trade(long price, long quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+        public long Price { get; set; }
+        public long Quantity { get; set; }
     }
 }
